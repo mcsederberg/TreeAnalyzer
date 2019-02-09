3cf94faaf118269fbd61ef649f6d85ee28011adc
mcsederberg <mcsederberg@gmail.com>
2019-02-08 19:26:27 -0700

    Updates

---
+++ precache-manifest.ed505147d72e85835d3fc94636b8f984.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fed1b48b50190e4d7f64",
+    "url": "/static/css/main.1985bc61.chunk.css"
+  },
+  {
+    "revision": "fed1b48b50190e4d7f64",
+    "url": "/static/js/main.fed1b48b.chunk.js"
+  },
+  {
+    "revision": "6673402819e54f7e09bc",
+    "url": "/static/css/1.c089dd7d.chunk.css"
+  },
+  {
+    "revision": "6673402819e54f7e09bc",
+    "url": "/static/js/1.66734028.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "24a5973231c862a079e3970e4ac5f0bc",
+    "url": "/index.html"
+  }
+];
